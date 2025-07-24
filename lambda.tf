24c232e89743432960bf729d1735db94285df6d0
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-25 01:44:05 +0800

    .

--- lambda.tf
+++ lambda.tf
@@ -1,18 +1,9 @@
 # Defines the lambda function code that will be zipped
 
-data "archive_file" "lambda" {
+data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "./files/${var.lambda_file_name}.mjs"
-  output_path = "./files/${var.lambda_file_name}.zip"
+  source_file = "${path.module}/lambda/hello.py"
+  output_path = "${path.module}/lambda/hello.zip"
 }
 
 # Defines the lambda function to be created using file
-
-resource "aws_lambda_function" "own_lambda" {
-  function_name    = var.lambda_function_name
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "nodejs22.x"
-  role              = aws_iam_role.iam_for_lambda.arn
-  filename         = data.archive_file.lambda.output_path                      # Adjust the path
-  source_code_hash = data.archive_file.lambda.output_base64sha256               # Adjust the path
-}
